720463cc8e36847bd6ac21c165659dc2145ae5a3
szilardguti <szilardguti@gmail.com>
2022-12-09 23:18:44 +0100

    add day 06 - marker finding (with better python naming conventionts)

---
+++ days/06/first.py
@@ -0,0 +1,22 @@
+def is_marker(four_digit_string):
+    for wanted_chr in four_digit_string:
+        count = 0
+        for got_chr in four_digit_string:
+            if wanted_chr == got_chr:
+                count = count + 1
+        if count > 1:
+            return False
+    return True
+
+with open("input.txt", "r") as f:
+    data_stream = f.read()
+    
+    buffer_size = 4
+    max_index = len(data_stream)-buffer_size+1
+
+    for i in range(max_index):
+        current_window = data_stream[i:i+buffer_size]
+
+        if(is_marker(current_window)):
+            print(i+buffer_size)
+            break
